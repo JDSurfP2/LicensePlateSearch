f7b5fa57afceb69f21852cd781c92e11a366b696
Jared Detro <jaredjdetro@gmail.com>
2020-05-26 16:06:41 -0600

    Initial Commit

---
+++ License Plate Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  License Plate Search
+//
+//  Created by Jared Detro on 5/26/20.
+//  Copyright © 2020 Jared Detro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
